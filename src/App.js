50e586d47434a56a0b1ebb48c6bbd0d9b3cb5e1a
harsh30896 <hars30896@gmail.com>
2024-10-31 18:49:29 +0530

    some css adding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+
 import React from 'react';
 import CreateUser from './components/CreateUser';
 
 function App() {
     return (
         <div className="App">
-            <h1>User Management</h1>
+            <h2>Event Crowd Management</h2>
             <CreateUser />
         </div>
     );
